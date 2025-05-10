0323e6076404c540c86f83734cd3a1569dafe9b1
0xFF <91343617+tester1010101@users.noreply.github.com>
2025-05-10 13:08:35 -0400

    Adding custom script utilities
    include.ps1 = creates structure to include multiple repeated number webpages [result/1, result/2, ... result/140]...
    exclude.ps1 = creates structure to exclude multiple custom criterias from crawls...
    
    can be tailored to your needs, dont hesitate to mod

---
+++ exclude.ps1
@@ -0,0 +1,59 @@
+﻿#####################################################################
+# Generate filters structure to exclude stuff from being crawled
+
+Write-Host -ForegroundColor Magenta "#### :: #### [ STARTING ] #### :: ####"
+Write-Host -ForegroundColor Magenta "#### :: #### [ Have you placed your AAAA.txt in CD\excludeLists folder? ] #### :: ####"
+pause
+
+$wFolder = (pwd).Path
+Write-Host -ForegroundColor DarkMagenta "Current directory = [$wFolder]"
+sleep 1
+
+
+[bool]$pathBool = (Test-Path -Path "$wFolder\excludeLists\AAAA.txt")
+if ($pathBool -eq $true)
+{
+    $collection = "$wFolder\excludeLists\AAAA.txt"
+}
+else
+{
+    mkdir $wFolder\excludeLists
+    Write-Host -ForegroundColor Red "$wFolder\excludeLists\AAAA.txt NOT FOUND, exiting in 3..."
+    sleep 1
+    Write-Host -ForegroundColor Red "$wFolder\excludeLists\AAAA.txt NOT FOUND, exiting in 2..."
+    sleep 1
+    Write-Host -ForegroundColor Red "$wFolder\excludeLists\AAAA.txt NOT FOUND, exiting in 1..."
+    sleep 1
+    pause
+    exit
+}
+
+sleep 1
+
+$string = $null
+$string2 = $null
+
+
+[array]$excludeF = @("`$diF = @(`"--exclude=```"(`", ``")
+
+foreach ($item in $collection)
+{
+    $string = "    `"$item`", ``"
+    Write-Host $string
+    [array]$excludeF += $string
+}
+
+
+$excludeF | out-file -encoding utf8 -filepath ./excludeFilter_import.txt
+# [array]$excludeF += "    `"action=history`", ``"
+#
+#    $diF = @(
+#    "https://www.gameye.app/encyclopedia/all?ps=100&p=1", `
+#    "https://www.gameye.app/encyclopedia/all?ps=100&p=2", `
+#    ...
+#    "https://www.gameeye.app/other")
+#
+#
+# iF = -> (^)
+
+#############################################################################
---
+++ include.ps1
@@ -0,0 +1,82 @@
+﻿################################
+# Webpages Inclusion Generator #
+# ---------------------------- #
+#  Generates pages numbers to  #
+# be crawled through according # # # # -> include page 1, 2, 3, 4, 5 ... 10. (add link-hops/include...)
+#  to chosen webpages portion. #
+# ---------------------------- #
+# made by: tester1010101 (v01) #
+# ---------------------------- #
+#############################################################################
+# TO-DO: choose url portion to modify/increment [https://page.with/complicated{$i}Links]
+# .>> Choose URL portion (foreach $[index] in $pageStr_URL, choose $index to increment/generate)
+
+<#
+
+Write-Host -ForegroundColor Magenta "#### :: #### [ STARTING -> {1.} bookmarks conversion ] #### :: ####"
+
+$wFolder = (pwd).Path
+Write-Host -ForegroundColor DarkMagenta "Output directory = [$wFolder]"
+sleep 1
+
+$bookMarks = "$wFolder\excludeLists\AAAA.txt"
+Write-Host -ForegroundColor DarkMagenta "Bookmark.file = [$bookMarks]"
+sleep 1
+
+
+$i = 0
+$startNum = $i
+$test1 = "https://github.com/tester1010101/p1/data"
+$endNum = $test1.Length
+
+do
+{
+    $test2 = $null
+    $test2 = $test1[$i]
+    Write-Host "$i. $test2"
+    $i++
+}
+until ($i -eq $endNum)
+
+Write-Host "Selexion? [$startNum >> $endNum]"
+$selectVal = Read-Host
+
+#>
+
+$includeF = $null
+$pageStr = $null
+$wPage = $null
+
+Write-Host "Page to increment? eg: [{https://www.google.com/results/}]"
+[string]$pageStr = Read-Host
+
+Write-Host "Start number (`$value)? [https://results.com/p/{`$value}] eg. {1}"
+[int]$nbStar = Read-Host
+
+Write-Host "Completion number (`$value)? [https://results.com/p/{`$value}] eg. {140}"
+Write-Host "Generates pages from number X to Y. {page1 --> page 140}"
+[string]$nbEnd = Read-Host
+
+[array]$includeF = @("`$diF = @(`"--include=```"(`", ``")
+
+do
+{
+    $wPage = "    `"$pageStr$nbStar`", ``"
+    Write-Host $wPage
+    [array]$includeF += $wPage
+    $i++
+}
+until ($nbStar -eq $nbEnd)
+
+
+$includeF | out-file -encoding utf8 -filepath "$wFolder\includeFilter_import.txt"
+##################################################################################
+#
+#    $diF = @(--include=", `
+#    "https://www.gameye.app/encyclopedia/all?ps=100&p=1", `
+#    "https://www.gameye.app/encyclopedia/all?ps=100&p=2", `
+#    ...
+#    "https://www.gameeye.app/other")
+#
+#
+# iF = -> (^)
